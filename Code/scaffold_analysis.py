427a9e9b7dc6848cc0bf31c4303ba075b25c04ca
Zachary Fralish <zachary.fralish@duke.edu>
2023-12-01 14:36:34 -0500

    Update scaffold_analysis.py
    Include External Test Set Evaluation

--- Code/scaffold_analysis.py
+++ Code/scaffold_analysis.py
@@ -71,6 +71,11 @@ datasets = ['CHEMBL1267245',
  'CHEMBL3889083',
  'CHEMBL3889139']
  
+ 
+########################
+### Cross-Validation ###
+########################
+ 
 
 all_scoresNM = pd.DataFrame(columns=['Dataset', 'Pearson\'s r RF', 'MAE RF', 'RMSE RF',
                                     'Pearson\'s r CP', 'MAE CP', 'RMSE CP', 'Pearson\'s r DD', 'MAE DD', 'RMSE DD']) # For pairs of Nonmatching Scaffolds
@@ -174,8 +179,118 @@ for dataset in datasets:
 
     all_scoresM = pd.concat([all_scoresM, scoringM])
 
-all_scoresNM.to_csv("DeepDelta_Scaffold_NonMatching.csv", index = False) # Save results for unmatched scaffold pairs
-all_scoresM.to_csv("DeepDelta_Scaffold_Matching.csv", index = False) # Save results for matched scaffold pairs
+all_scoresNM.to_csv("DeepDelta_CV_Scaffold_NonMatching.csv", index = False) # Save results for unmatched scaffold pairs
+all_scoresM.to_csv("DeepDelta_CV_Scaffold_Matching.csv", index = False) # Save results for matched scaffold pairs
 
 #########################
 
+
+
+
+##########################
+### External Test Sets ###
+##########################
+ 
+
+all_scoresNM = pd.DataFrame(columns=['Dataset', 'Pearson\'s r RF', 'MAE RF', 'RMSE RF',
+                                    'Pearson\'s r CP', 'MAE CP', 'RMSE CP', 'Pearson\'s r DD', 'MAE DD', 'RMSE DD']) # For pairs of Nonmatching Scaffolds
+all_scoresM = pd.DataFrame(columns=['Dataset', 'Pearson\'s r RF', 'MAE RF', 'RMSE RF',
+                                    'Pearson\'s r CP', 'MAE CP', 'RMSE CP', 'Pearson\'s r DD', 'MAE DD', 'RMSE DD']) # For pairs with Matching Scaffolds
+
+# Evaluate Matching and Non-matching Scaffold Pairs for all Datasets
+for dataset in datasets:
+    dataframe = pd.read_csv('../Datasets/Test/{}_test.csv'.format(dataset)) # External Test Sets
+
+    # 3 Models to Evaluate
+    # 1 - Random Forest 
+    predictions_RF = pd.read_csv('../Results/External_Test_Results/RandomForest_Ext_Test/{}_RandomForest_Test.csv'.format(dataset)).T
+    predictions_RF.columns =['True', 'Delta']
+    # 2 - ChemProp
+    predictions_CP = pd.read_csv('../Results/External_Test_Results/ChemProp50_Ext_Test/{}_ChemProp50_Test.csv'.format(dataset)).T
+    predictions_CP.columns =['True', 'Delta']
+    # 3 - DeepDelta
+    predictions_DD = pd.read_csv('../Results/External_Test_Results/DeepDelta5_Ext_Test/{}_DeepDelta5_Test.csv'.format(dataset)).T
+    predictions_DD.columns =['True', 'Delta']
+
+    # Prepare Scaffolds
+    mols = [Chem.MolFromSmiles(s) for s in dataframe.SMILES]
+    scaffolds = [Chem.MolToSmiles(MurckoScaffold.GetScaffoldForMol(m)) for m in mols]
+    data = pd.DataFrame(data={'Scaffold':  scaffolds})
+    del dataframe
+
+    # Cross-Merge the External Test Sets and Extract the scaffolds
+    datapoint_x = []
+    datapoint_y = []
+
+    pair_subset_test = pd.merge(data, data, how='cross')
+    datapoint_x += [pair_subset_test.Scaffold_x]
+    datapoint_y += [pair_subset_test.Scaffold_y]
+    del pair_subset_test
+
+    datapoints = pd.DataFrame(data={'X':  np.concatenate(datapoint_x), 'Y':  np.concatenate(datapoint_y)})
+
+    # Add the actual deltas and predicted deltas
+    trues = predictions_CP['True']
+    trues = [float(i) for i in trues]
+    datapoints['True'] = trues
+
+    DeltasRF = predictions_RF['Delta']
+    DeltasRF = [float(i) for i in DeltasRF]
+    datapoints['DeltaRF'] = DeltasRF
+
+    DeltasCP = predictions_CP['Delta']
+    DeltasCP = [float(i) for i in DeltasCP]
+    datapoints['DeltaCP'] = DeltasCP
+
+    DeltasDD = predictions_DD['Delta']
+    DeltasDD = [float(i) for i in DeltasDD]
+    datapoints['DeltaDD'] = DeltasDD
+
+    # Grab the datapoints with matching scaffolds 
+    matching = datapoints[datapoints['X'] == datapoints['Y']]
+    
+    # Grab the nonmatching datapoints
+    nonmatching = datapoints[datapoints['X'] != datapoints['Y']]
+
+    # Run Stats - Non-matching Scaffolds
+    pearson_NM_RF = stats.pearsonr(nonmatching["True"], (nonmatching['DeltaRF']))
+    MAE_NM_RF = metrics.mean_absolute_error(nonmatching["True"], (nonmatching['DeltaRF']))
+    RMSE_NM_RF = math.sqrt(metrics.mean_squared_error(nonmatching["True"], (nonmatching['DeltaRF'])))
+
+    pearson_NM_CP = stats.pearsonr(nonmatching["True"], (nonmatching['DeltaCP']))
+    MAE_NM_CP = metrics.mean_absolute_error(nonmatching["True"], (nonmatching['DeltaCP']))
+    RMSE_NM_CP = math.sqrt(metrics.mean_squared_error(nonmatching["True"], (nonmatching['DeltaCP'])))
+
+    pearson_NM_DD = stats.pearsonr(nonmatching["True"], (nonmatching['DeltaDD']))
+    MAE_NM_DD = metrics.mean_absolute_error(nonmatching["True"], (nonmatching['DeltaDD']))
+    RMSE_NM_DD = math.sqrt(metrics.mean_squared_error(nonmatching["True"], (nonmatching['DeltaDD'])))
+
+    scoringNM = pd.DataFrame({'Dataset': [dataset], 'Pearson\'s r RF': [round(pearson_NM_RF[0], 4)], 'MAE RF': [round(MAE_NM_RF, 4)], 'RMSE RF': [round(RMSE_NM_RF, 4)],
+                            'Pearson\'s r CP': [round(pearson_NM_CP[0], 4)], 'MAE CP': [round(MAE_NM_CP, 4)], 'RMSE CP': [round(RMSE_NM_CP, 4)],
+                            'Pearson\'s r DD': [round(pearson_NM_DD[0], 4)], 'MAE DD': [round(MAE_NM_DD, 4)], 'RMSE DD': [round(RMSE_NM_DD, 4)]})
+
+    all_scoresNM = pd.concat([all_scoresNM, scoringNM])
+
+    # Run Stats - Matching Scaffolds
+    pearson_M_RF = stats.pearsonr(matching["True"], (matching['DeltaRF']))
+    MAE_M_RF = metrics.mean_absolute_error(matching["True"], (matching['DeltaRF']))
+    RMSE_M_RF = math.sqrt(metrics.mean_squared_error(matching["True"], (matching['DeltaRF'])))
+
+    pearson_M_CP = stats.pearsonr(matching["True"], (matching['DeltaCP']))
+    MAE_M_CP = metrics.mean_absolute_error(matching["True"], (matching['DeltaCP']))
+    RMSE_M_CP = math.sqrt(metrics.mean_squared_error(matching["True"], (matching['DeltaCP'])))
+
+    pearson_M_DD = stats.pearsonr(matching["True"], (matching['DeltaDD']))
+    MAE_M_DD = metrics.mean_absolute_error(matching["True"], (matching['DeltaDD']))
+    RMSE_M_DD = math.sqrt(metrics.mean_squared_error(matching["True"], (matching['DeltaDD'])))
+
+    scoringM = pd.DataFrame({'Dataset': [dataset], 'Pearson\'s r RF': [round(pearson_M_RF[0], 4)], 'MAE RF': [round(MAE_M_RF, 4)], 'RMSE RF': [round(RMSE_M_RF, 4)],
+                            'Pearson\'s r CP': [round(pearson_M_CP[0], 4)], 'MAE CP': [round(MAE_M_CP, 4)], 'RMSE CP': [round(RMSE_M_CP, 4)],
+                            'Pearson\'s r DD': [round(pearson_M_DD[0], 4)], 'MAE DD': [round(MAE_M_DD, 4)], 'RMSE DD': [round(RMSE_M_DD, 4)]})
+
+    all_scoresM = pd.concat([all_scoresM, scoringM])
+
+all_scoresNM.to_csv("DeepDelta_Ext_Scaffold_NonMatching.csv", index = False) # Save results for unmatched scaffold pairs
+all_scoresM.to_csv("DeepDelta_Ext_Scaffold_Matching.csv", index = False) # Save results for matched scaffold pairs
+
+#########################
